2ab5f91e9139920f6ef068150816c4743e78d5a4
Tam <tacs.laboral@gmail.com>
2024-07-21 22:25:33 -0400

    componemte list terminado

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,40 +1,48 @@
-
-const List = () => {
+import datadbz from '../img/datadbz.jpg';
+import interactive from '../img/interactive.jpg';
+import moviegeek from '../img/moviegeek.jpg';
+import memoria from '../img/memoria.jpg';
+  //falta arreglar las imagenes de cada proyecto, la info y que tenga la alineacion que entrega tailwind
+  const List = () => {
     return (
-        <div className="row row-cols-1 row-cols-md-2 g-4">
-        <div className="col">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div className="flex flex-wrap -m-4">
+        <div className="p-4 w-full md:w-1/2 lg:w-1/3">
+          <div className="card bg-white rounded-lg shadow-lg overflow-hidden">
+            <img src={datadbz} className="w-full h-32 object-cover" alt="datadbz" />
+            <div className="card-body p-4">
+              <h5 className="card-title text-xl font-bold mb-2">App Data Lovers_DragoBall</h5>
+              <p className="card-text text-gray-700">Plataforma en línea con una interfaz de tarjetas que permite explorar, filtrar y ordenar datos según las preferencias del usuario.
+
+</p>
             </div>
           </div>
         </div>
-        <div className ="col">
-          <div className = "card">
-            <img src="..." className ="card-img-top" alt="..."/>
-            <div className ="card-body">
-              <h5 className ="card-title">Card title</h5>
-              <p className ="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div className="p-4 w-full md:w-1/2 lg:w-1/3">
+          <div className="card bg-white rounded-lg shadow-lg overflow-hidden">
+            <img src={interactive} className="w-full h-32 object-cover" alt="interactive" />
+            <div className="card-body p-4">
+              <h5 className="card-title text-xl font-bold mb-2">Interactive Comments</h5>
+              <p className="card-text text-gray-700">Sección de comentarios interactiva en un entorno web, mejorando la funcionalidad para coincidir con el diseño proporcionado. <br></br>Desafío de Front-mentor.
+
+</p>
             </div>
           </div>
         </div>
-        <div className="col">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <div className="p-4 w-full md:w-1/2 lg:w-1/3">
+          <div className="card bg-white rounded-lg shadow-lg overflow-hidden">
+            <img src={moviegeek} className="w-full h-32 object-cover" alt="moviegeek" />
+            <div className="card-body p-4">
+              <h5 className="card-title text-xl font-bold mb-2">App Movie Geek</h5>
+              <p className="card-text text-gray-700">Plataforma de películas que brinda a los usuarios la posibilidad de descubrir contenido según sus gustos personales, ofreciendo año y título.</p>
             </div>
           </div>
         </div>
-        <div className="col">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div className="p-4 w-full md:w-1/2 lg:w-1/3">
+          <div className="card bg-white rounded-lg shadow-lg overflow-hidden">
+            <img src={memoria} className="w-full h-32 object-cover" alt="memoria" />
+            <div className="card-body p-4">
+              <h5 className="card-title text-xl font-bold mb-2">App Juego Memoria Nórdica</h5>
+              <p className="card-text text-gray-700">Juego de cartas Memoria Nórdica desafiará tu destreza mental mientras encuentras las parejas de símbolos vikingos iguales para completar el tablero.</p>
             </div>
           </div>
         </div>
@@ -43,4 +51,3 @@ const List = () => {
   };
   
   export default List;
-  //falta arreglar las imagenes de cada proyecto, la info y que tenga la alineacion que entrega tailwind
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import Navbar from './components/Nav.jsx'
 import Footer from './components/Footer.jsx'
-import Card from './components/Card.jsx'
+// import Card from './components/Card.jsx'
 import List from './components/List.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
-    <Card />
+    
     <List />
     <App />
     <Footer />
