74c9529f4c396e7f4d3e531ef57655d47d5466e7
Tam <tacs.laboral@gmail.com>
2024-09-23 10:23:50 -0300

    nav dm

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false); // Estado para controlar si el menú está abierto o cerrado
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); // Alternar el estado abierto/cerrado del menú
+  };
+
   return (
-    <nav className="w-full p-4  fixed top-0">
-      <ul className="flex justify-center space-x-4">
+    <nav className="w-full p-4 bg-white fixed top-0 z-10 shadow-md">
+      {/* Botón "hamburger" visible solo en pantallas pequeñas */}
+      <div className="flex justify-between items-center md:hidden">
+        <a href="#home" className="text-black font-bold">
+          Logo
+        </a>
+        <button
+          onClick={toggleMenu}
+          className="text-black focus:outline-none focus:text-gray-700"
+        >
+          {/* Icono "hamburger" */}
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+      </div>
+
+      {/* Menú en pantallas medianas y grandes */}
+      <ul
+        className={`${
+          isOpen ? "block" : "hidden"
+        } md:flex md:justify-center md:space-x-4 mt-4 md:mt-0`}
+      >
         <li className="nav-item">
-          <a className="text-black hover:text-gray-200" href="#home">
+          <a className="text-black hover:text-gray-300" href="#home">
             Home
           </a>
         </li>
